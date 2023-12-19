8ba18dc8fcf3dac3f9244b53fe8ab55cc2dd947a
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2023-12-19 09:38:45 -0800

    create sign in api route and format

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { test } from '../controller/user.controller.js';
+import express from "express";
+import { test } from "../controller/user.controller.js";
 
 const router = express.Router();
 
-router.get('/test', test);
+router.get("/test", test);
 
 export default router;
