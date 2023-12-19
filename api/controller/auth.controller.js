5759feff560829e12ed970dbcf95c36af626a8cf
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2023-12-19 10:16:38 -0800

    finish sign in page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -20,11 +20,11 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      return next(errorHandler(400, "User not found"));
+      return next(errorHandler(404, "User not found"));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-      return next(errorHandler(400, "Wrong credentials"));
+      return next(errorHandler(401, "Wrong credentials"));
     }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: userPassword, ...rest } = validUser._doc;
