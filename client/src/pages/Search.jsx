2dac02584ac3c928ff9766edf54988f15c97d13c
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2023-12-27 17:10:08 -0800

    add functionality to search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,63 +1,212 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    offer: false,
+    parking: false,
+    furnished: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const offerFromUrl = urlParams.get("offer");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      offerFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        offer: offerFromUrl === "true" ? true : false,
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+    const fetchListings = async () => {
+        setLoading(true);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        setListings(data);
+        setLoading(false);
+    };
+    fetchListings();
+  }, [location.search]);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "independent" ||
+      e.target.id === "shared"
+    ) {
+      setSidebardata({ ...sidebardata, type: e.target.id });
+    }
+    if (e.target.id === "searchTerm") {
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+    }
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+      const order = e.target.value.split("_")[1] || "desc";
+      setSidebardata({ ...sidebardata, sort, order });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-    <div className='flex flex-col md:flex-row'>
-        <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
-            <form className='flex flex-col gap-8'>
-                <div className='flex items-center gap-2'>
-                    <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                    <input type="text" id='searchTerm' placeholder='Search...' className='border rounded-lg p-3 w-full '/>
-                </div>
-                <div className='flex gap-2 flex-wrap items-center'>
-                    <label className='font-semibold'>Type:</label>
-                    <div>
-                        <input type="checkbox" id='all' className='w-5' />
-                        <span>Independ & Shared</span>
-                    </div>
-                    <div>
-                        <input type="checkbox" id='independent' className='w-5' />
-                        <span>Independ</span>
-                    </div>
-                    <div>
-                        <input type="checkbox" id='shared' className='w-5' />
-                        <span>Shared</span>
-                    </div>
-                    <div>
-                        <input type="checkbox" id='offer' className='w-5' />
-                        <span>Offer</span>
-                    </div>
-                </div>
-                <div className='flex gap-2 flex-wrap items-center'>
-                    <label className='font-semibold'>Amenities:</label>
-                    <div>
-                        <input type="checkbox" id='parking' className='w-5' />
-                        <span>Parking</span>
-                    </div>
-                    <div>
-                        <input type="checkbox" id='furnished' className='w-5' />
-                        <span>Furnished</span>
-                    </div>
-                </div>
-                <div className='flex items-center gap-2'>
-                    <label className='font-semibold'>Sort:</label>
-                    <select id="sort_order" className='border rounded-lg p-3'>
-                        <option> Price high to low</option>
-                        <option> Price low to high</option>
-                        <option> Latest</option>
-                        <option> Oldest</option>
-                    </select>
-                </div>
-                <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
-
-
-            </form>
-
-        </div>
-        <div className=''>
-            <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
-
-        </div>
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
+            <input
+              type="text"
+              id="searchTerm"
+              placeholder="Search..."
+              className="border rounded-lg p-3 w-full "
+              value={sidebardata.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold">Type:</label>
+            <div>
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type === "all"}
+              />
+              <span>Independ & Shared</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="independent"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type === "independent"}
+              />
+              <span>Independ</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="shared"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type === "shared"}
+              />
+              <span>Shared</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.offer === true}
+              />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className="flex gap-2 flex-wrap items-center">
+            <label className="font-semibold">Amenities:</label>
+            <div>
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.parking === true}
+              />
+              <span>Parking</span>
+            </div>
+            <div>
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.furnished === true}
+              />
+              <span>Furnished</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Sort:</label>
+            <select
+              id="sort_order"
+              className="border rounded-lg p-3"
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
+            >
+              <option value="regularPrice_desc"> Price high to low</option>
+              <option value="regularPrice_asc"> Price low to high</option>
+              <option value="createdAt_desc"> Latest</option>
+              <option value="createdAt_asc"> Oldest</option>
+            </select>
+          </div>
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="">
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
+          Listing results:
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
