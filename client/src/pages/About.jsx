ba8ee4a8ab138dd35314109c109c96b346aaefd1
Rui Lu <122314440+laughtale678@users.noreply.github.com>
2023-12-29 13:59:36 -0800

    add pre installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About UniRentFinder</h1>
+      <p className='mb-4 text-slate-700'>UniRentFinder is a platform designed specifically for university/college students, aiming to facilitate the sharing of rental and subletting information. We are committed to providing a convenient and reliable space for students to find their ideal accommodation while fostering the sharing of rental and subletting information within the university community.</p>
+      <p className='mb-4 text-slate-700'>
+      The mission of UniRentFinder is to create a shared community where students can easily discover housing that meets their needs and engage in the exchange of rental and subletting information on campus. We believe that through mutual assistance, students can more conveniently address housing challenges, creating a more harmonious and friendly living environment on campus.
+      </p>
+      <p className='mb-4 text-slate-700'>The UniRentFinder team is comprised of a passionate group of young individuals dedicated to the university/college student community. Understanding the housing challenges faced during the university years, we are committed to providing solutions for our peers. Our team consists of real estate professionals, tech developers, and social media experts working collaboratively to establish a secure, efficient, and interactive platform for student housing. We firmly believe that through the collective efforts of our team, UniRentFinder will become the preferred platform for students to share rental and subletting information.</p>
+    </div>
   )
 }
